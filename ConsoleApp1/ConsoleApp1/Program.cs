8edb7b760ffcd987a85a8bd64dd61eab0fda0389
aamir <wasi3aamir@gmail.com>
2019-02-07 00:10:27 +0600

    Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 1; t <= T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split('\n');
+                string pattern = @"\bSharif\b";
+                string replace = "Officer";
+                string result = Regex.Replace(line, pattern, replace);
+                Console.WriteLine($"Case {t}: {result}");
+            }
+        }
+    }
+}
